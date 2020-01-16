e743a1185e3c7868f627c9299cea1f4094a0179a
Jamison Cozart and Mariam Alaeddin <jamison@>
2020-01-16 10:17:18 -0800

    add conditionals to hide or show rides based off of height

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    var height = $("input#height").val();
+    console.log(height);
+    if(height < 4) {
+      $("#small").show();
+      $("#medium").hide();
+      $("#large").hide();
+    }else if(height >= 4 && height <= 5){
+      $("#medium").show();
+      $("#small").show();
+      $("#large").hide();  
+    }else if(height >5) {
+      $("#large").show();
+      $("#medium").show();
+      $("#small").show();
+    }
+
+
+
+    event.preventDefault();
+  });
+});
